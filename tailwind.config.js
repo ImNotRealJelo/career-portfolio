5e584a27a1072ea02c9cde49a37bba6941a39b89
Jihlo <33950312+ImNotRealJelo@users.noreply.github.com>
2022-03-11 17:34:18 +0800

    Fixes
    fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,13 @@ module.exports = {
         bounce: 'bounce 4s ease-in-out infinite',
       },
       colors: {
-        'body': '#F7F8F3',
-        'selected-text': '#7EE081',
-        'theme': '#7EE081',
+        'body': '#FBFFFE',
+        'selected-text': '#FAA916',
+        'theme': '#FAA916',
         'nav': '#2A282A',
-        'secondary': '#62A87C',
+        'secondary': '#F9BD4E',
         'badge': '#726D74',
-        'input-border': '#62A87C',
+        'input-border': '#726D74',
         'input': '#2A282A',
         'dark': '#2A282A'
       },
