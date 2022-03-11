84e9526c230863ad3fe5fc21db71ee6241a5a6ed
ImNotRealJelo <alviarjohnangelo@gmail.com>
2022-03-11 17:23:25 +0800

    asd

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,13 @@ module.exports = {
         bounce: 'bounce 4s ease-in-out infinite',
       },
       colors: {
-        'body': '#FBFFFE',
-        'selected-text': '#FAA916',
-        'theme': '#FAA916',
+        'body': '#F7F8F3',
+        'selected-text': '#7EE081',
+        'theme': '#7EE081',
         'nav': '#2A282A',
-        'secondary': '#F9BD4E',
+        'secondary': '#62A87C',
         'badge': '#726D74',
-        'input-border': '#726D74',
+        'input-border': '#62A87C',
         'input': '#2A282A',
         'dark': '#2A282A'
       },
